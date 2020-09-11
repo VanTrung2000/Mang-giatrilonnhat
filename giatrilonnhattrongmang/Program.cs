95c934b7d23ddfcbd22ce0f1e614412815038505
VanTrung2000 <vantrungdeptrai2000@gmail.com>
2020-09-11 10:02:08 +0700

    Add project files.

---
+++ giatrilonnhattrongmang/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace giatrilonnhattrongmang
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size;
+            int[] array;
+            do
+            {
+                Console.WriteLine("Enter a size:");
+                size = Int32.Parse(Console.ReadLine());
+                if (size > 20)
+                    Console.WriteLine("Size should not exceed 20");
+            } while (size > 20);
+            array = new int[size];
+            int i = 0;
+            while (i < array.Length)
+            {
+                Console.WriteLine("Enter element" + (i + 1) + " : ");
+                array[i] = Int32.Parse(Console.ReadLine());
+                i++;
+            }
+            Console.WriteLine("Property list: ");
+            for (int j = 0; j < array.Length; j++)
+            {
+                Console.WriteLine(array[j] + "\t");
+            }
+            int index = ko(array);
+            Console.WriteLine("gia tri lon nhat la  " + index);
+            Console.ReadLine();
+        }
+
+        private static int ko(int[] array)
+        {
+            int max = array[0];
+            int index = 1;
+            for (int j = 0; j < array.Length; j++)
+            {
+                if (array[j] > max)
+                {
+                    max = array[j];
+                    index = array[j] + 1;
+                }
+            }
+            return max;
+        }
+    }
+}
